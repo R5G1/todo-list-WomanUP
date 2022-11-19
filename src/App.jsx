cfe20da062e37b5e0959b17c0a4a48ba693036d7
R5G1 <vlad.klimachevski@gmail.com>
2022-11-19 16:35:25 +0300

    init: start todo-list-task

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.scss';
+// import ReactForm from './pages/React-forms/forms';
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    return <div className="App">{/* <ReactForm /> */}</div>;
+  }
+}
+
+export default App;
---
+++ src/index.jsx
@@ -0,0 +1,10 @@
+import ReactDOM from 'react-dom';
+import '../src/styles/index.scss';
+import App from './App';
+
+ReactDOM.render(
+  <div>
+    <App />
+  </div>,
+  document.getElementById('root')
+);
