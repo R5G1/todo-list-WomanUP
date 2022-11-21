3dc5f43399703112f61d5ea18737b5a8bf3152c9
R5G1 <vlad.klimachevski@gmail.com>
2022-11-21 16:16:54 +0300

    fix: dataCalc

--- src/pages/React-forms/form-components/componets/dataCalc.jsx
+++ src/pages/React-forms/form-components/componets/dataCalc.jsx
@@ -2,6 +2,9 @@ function DataCalc(dateNow, data) {
   const arrayDataNow = dateNow.split('.');
   const arrayDataAfter = data.split('-').reverse();
 
+  if (arrayDataAfter[2] > arrayDataNow[2]) {
+    return true;
+  }
   if (arrayDataNow[0] > arrayDataAfter[0]) {
     if (arrayDataNow[1] >= arrayDataAfter[1]) {
       return false;
